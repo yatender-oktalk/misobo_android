fd7af1f3ed407193efa97baf088cedbc6df6023c
Ankit <nktkumar82@gmail.com>
2020-10-17 00:06:53 +0530

    style:Complete OnBoarding 2nd page UI.

--- app/src/main/java/com/example/misobo/MainActivity.kt
+++ app/src/main/java/com/example/misobo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.misobo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedText.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.onBoardingFrameContainer, MisoboMembersFragment())
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+        }
+
     }
 }
