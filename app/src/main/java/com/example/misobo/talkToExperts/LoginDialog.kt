1a3511c9e52d58132705566586c94804b91baa9d
Ankit <nktkumar82@gmail.com>
2020-11-17 19:51:03 +0530

    RxBinding added for textChanges.

--- app/src/main/java/com/example/misobo/talkToExperts/LoginDialog.kt
+++ app/src/main/java/com/example/misobo/talkToExperts/LoginDialog.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.misobo.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jakewharton.rxbinding2.widget.RxTextView
+import kotlinx.android.synthetic.main.login_bottom_sheet.*
 
-class LoginDialog:BottomSheetDialogFragment() {
+class LoginDialog : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +19,27 @@ class LoginDialog:BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.login_bottom_sheet, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        RxTextView.textChanges(phonenumberText)
+            .subscribe {
+               if (it.length >= 10) {
+                   sendOtpButton.alpha = 1f
+                   sendOtpButton.isEnabled = true
+               }else{
+                   sendOtpButton.alpha = .7f
+                   sendOtpButton.isEnabled = false
+               }
+            }
+
     }
 
     override fun setupDialog(dialog: Dialog, style: Int) {
