9f939fd3edec8c1ef1fb7916de4f8b1a2edb6c66
Ankit <nktkumar82@gmail.com>
2020-11-20 23:05:56 +0530

    Home Page UI complete.

--- app/src/main/java/com/example/misobo/mind/items/TalkToTherapistItem.kt
+++ app/src/main/java/com/example/misobo/mind/items/TalkToTherapistItem.kt
@@ -1,11 +1,28 @@
 package com.example.misobo.mind.items
 
 import com.example.misobo.R
+import com.example.misobo.talkToExperts.items.ExpertsRecyclerItem
+import com.example.misobo.talkToExperts.models.ExpertModel
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.talk_to_therapist_item.view.*
+import kotlinx.android.synthetic.main.tasks_for_the_day_layout.view.*
 
-class TalkToTherapistItem : Item() {
+class TalkToTherapistItem(val expertList: ExpertModel) : Item() {
+    val adapter = GroupAdapter<ViewHolder>()
+
     override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.expertsRecyclerView.adapter = adapter
+        adapter.clear()
+        val expertsSection = Section()
+        expertList.entries?.take(3)?.forEach { model->
+            expertsSection.add(ExpertsRecyclerItem(model){
+
+            })
+        }
+        adapter.add(expertsSection)
     }
 
     override fun getLayout(): Int = R.layout.talk_to_therapist_item
--- app/src/main/java/com/example/misobo/mind/view/MindFragment.kt
+++ app/src/main/java/com/example/misobo/mind/view/MindFragment.kt
@@ -1,18 +1,21 @@
 package com.example.misobo.mind.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.misobo.R
 import com.example.misobo.mind.items.HelloItem
+import com.example.misobo.mind.items.TalkToTherapistItem
 import com.example.misobo.mind.items.TasksForTheDayItems
 import com.example.misobo.mind.viewModels.MindViewModel
 import com.example.misobo.mind.viewModels.MusicFetchState
+import com.example.misobo.talkToExperts.viewModels.ExpertListState
+import com.example.misobo.talkToExperts.viewModels.TalkToExpertsViewModel
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Section
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
@@ -21,9 +24,11 @@ import kotlinx.android.synthetic.main.fragment_mind.*
 class MindFragment : Fragment() {
 
     private val mindViewModel: MindViewModel by activityViewModels()
+    private val talkToExpertsViewModel by lazy { ViewModelProvider(this).get(TalkToExpertsViewModel::class.java) }
     private val adapter = GroupAdapter<ViewHolder>()
     private val helloSection = Section()
     private val taskForTheDaySection = Section()
+    private val talkToExpertsSection = Section()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,12 +43,27 @@ class MindFragment : Fragment() {
 
         mindHomeRecyclerView.adapter = adapter
         helloSection.add(HelloItem())
+
+        talkToExpertsViewModel.getAllExpertsList()
+        talkToExpertsViewModel.expertListLiveData.observe(viewLifecycleOwner, Observer { state->
+            when(state){
+                is ExpertListState.Success->{
+                    talkToExpertsSection.add(TalkToTherapistItem(state.expertList))
+                }
+                is ExpertListState.Fail->{
+
+                }
+                is ExpertListState.Loading->{
+
+                }
+            }
+        })
+
         mindViewModel.fetchAllMusic()
         mindViewModel.musicLiveData.observe(viewLifecycleOwner, Observer { state ->
             when (state) {
                 is MusicFetchState.Success -> {
                     taskForTheDaySection.add(TasksForTheDayItems(state.musicEntries))
-                    Log.i("state", state.musicEntries.entries?.size.toString())
                 }
                 is MusicFetchState.Loading -> {
 
@@ -57,6 +77,7 @@ class MindFragment : Fragment() {
         adapter.apply {
             add(helloSection)
             add(taskForTheDaySection)
+            add(talkToExpertsSection)
         }
     }
 }
