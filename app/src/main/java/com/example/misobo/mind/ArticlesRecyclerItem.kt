57144dcc2ad5fc994c8322285d5095e6cf8e0902
Ankit <nktkumar82@gmail.com>
2020-11-20 20:46:00 +0530

    code refactored.

--- app/src/main/java/com/example/misobo/mind/ArticlesRecyclerItem.kt
+++ app/src/main/java/com/example/misobo/mind/ArticlesRecyclerItem.kt
@@ -1,14 +1,13 @@
 package com.example.misobo.mind
 
+import com.example.misobo.R
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 
-class ArticlesRecyclerItem:Item() {
+class ArticlesRecyclerItem : Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
-    override fun getLayout(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getLayout(): Int = R.layout.curated_articles_recycler_item
 }
