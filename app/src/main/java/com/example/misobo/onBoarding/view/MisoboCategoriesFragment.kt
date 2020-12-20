ea2966cb837cafa4220eb557842014ca523d8585
Ankit <nktkumar82@gmail.com>
2020-12-20 22:41:25 +0530

    Horoscope bug fixed.

--- app/src/main/java/com/example/misobo/onBoarding/view/MisoboCategoriesFragment.kt
+++ app/src/main/java/com/example/misobo/onBoarding/view/MisoboCategoriesFragment.kt
@@ -21,16 +21,6 @@ import com.xwray.groupie.Section
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.fragment_misobo_categories.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MisoboCategoriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MisoboCategoriesFragment : Fragment() {
 
     val groupAdapter = GroupAdapter<ViewHolder>()
@@ -45,18 +35,6 @@ class MisoboCategoriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_misobo_categories, container, false)
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MisoboCategoriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
